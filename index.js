4172098c6e6685f345e8f2de132a6d0594d220ab
Christofer Chan <devchumbing@gmail.com>
2018-05-23 22:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ function deepestChild() {
 }
 
 function increasRankBy(n){
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(let i = 0; i < lis.length)
 }
