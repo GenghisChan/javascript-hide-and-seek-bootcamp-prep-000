7ef83c6429891c333a5e1a5163e2a28a0bcfc0f8
Christofer Chan <devchumbing@gmail.com>
2018-05-23 22:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(x) {
  return document.querySelector(x)
 }
 
-function nestedTarget(x){
+function nestedTarget(){
   return document.getElementById('nested').querySelectorAll('div.target')
 }
