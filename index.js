d5749864416120bf2b6f3b446fa62403d62ee677
Christofer Chan <devchumbing@gmail.com>
2018-05-23 22:38:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(x) {
+ return document.querySelector(x)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild() {
+  const lis = document.getElementById('grand-node').querySelectorAll('div')
+  return lis[3]
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
